a95657f7604854b00d590010b5d024eb692120b3
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-05 16:20:50 +0300

    add markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+
+function makeGalleryItemsMarkup(galleryItems) {
+   const GalleryItemsMarkup =  galleryItems.map(({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href= "${original}">
+    <img
+      class="gallery__image"
+      src= "${preview}"
+      data-source= ${original}
+      alt= "${description}"
+    />
+  </a>
+</div>`).join('')
+   
+    galleryEl.insertAdjacentHTML('beforeend', GalleryItemsMarkup);
+}
+
+ makeGalleryItemsMarkup(galleryItems);
+
+
+
+
+
 
 console.log(galleryItems);
