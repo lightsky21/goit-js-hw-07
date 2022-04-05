4d86fc56ad706a1da7baa23831c09907a35fed95
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-05 17:29:13 +0300

    добавляет разметку (задание2)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+function makeGalleryItemsMarkup(galleryItems) {
+    const GalleryItemsMarkup = galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join('')
+   
+    galleryEl.insertAdjacentHTML('beforeend', GalleryItemsMarkup);
+}
+
+ makeGalleryItemsMarkup(galleryItems);
