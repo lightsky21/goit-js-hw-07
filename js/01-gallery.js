b2a2d77ae4fce997f7ab3808487f1539bd866e11
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-05 19:54:26 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,7 +8,7 @@ const galleryEl = document.querySelector('.gallery');
 
 
 function makeGalleryItemsMarkup(galleryItems) {
-   const GalleryItemsMarkup =  galleryItems.map(({ preview, original, description }) => `<div class="gallery__item">
+   return galleryItems.map(({ preview, original, description }) => `<div class="gallery__item">
   <a class="gallery__link" href= "${original}">
     <img
       class="gallery__image"
@@ -18,13 +18,11 @@ function makeGalleryItemsMarkup(galleryItems) {
     />
   </a>
 </div>`).join('')
-   
-    galleryEl.insertAdjacentHTML('beforeend', GalleryItemsMarkup);
 }
 
- makeGalleryItemsMarkup(galleryItems);
+ const GalleryItemsMarkup = makeGalleryItemsMarkup(galleryItems);
 
-
+galleryEl.insertAdjacentHTML('beforeend', GalleryItemsMarkup);
 
 galleryEl.addEventListener('click', onGalleryElClick)
 
